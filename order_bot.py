d648ea61ad752941efaf8747061807b696c8f4e7
2dirtyjune <ulyssesmanuel13@gmail.com>
2025-10-29 15:47:11 -0500

    Update order_bot.py

--- order_bot.py
+++ order_bot.py
@@ -686,16 +686,14 @@ async def main():
     app.add_handler(CommandHandler("faq", faq))
     app.add_handler(CommandHandler("mustread", mustread))
 
-    await app.initialize()
-    await app.start()
-    print("💤 Bot still running...")
-    await app.updater.start_polling()
-    await app.updater.idle()
+print("💤 Bot still running...")
+await app.run_polling()
+
 
 if __name__ == "__main__":
-    import asyncio
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(main())
+    asyncio.run(main())
+
+
 
 
 
